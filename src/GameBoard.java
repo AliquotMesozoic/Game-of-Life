e7dd31409f06f2b2cd7b741d64fc72b81a1fe10c
Michael Trittin <render.still@gmail.com>
2014-10-02 16:22:09 -0400

    Adding a GameBoard to contain the cells

---
+++ src/GameBoard.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author trittimo
+ *
+ */
+public class GameBoard {
+	private ArrayList<Cell> cells = new ArrayList<Cell>();
+
+	private int rows;
+	private int columns;
+
+	public GameBoard(int rows, int columns) {
+
+	}
+}
